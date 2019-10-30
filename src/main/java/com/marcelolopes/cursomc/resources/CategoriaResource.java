3c1acfda73555f24e19e0994d2826929aed7480b
marcelolopesbr <marcelo.lopes@gmail.com>
2019-10-30 18:40:22 -0300

    Primeiro teste com REST

---
+++ src/main/java/com/marcelolopes/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,24 @@
+package com.marcelolopes.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcelolopes.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		ArrayList<Categoria> categorias = new ArrayList<>();
+		categorias.add(new Categoria(1,"Informática"));
+		categorias.add(new Categoria(1,"Escritório"));		
+		return categorias;
+	}
+	
+}
