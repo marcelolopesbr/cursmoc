c46814ede9f92a957caaaf2379eee9737c26435a
marcelolopesbr <marcelo.lopes@gmail.com>
2019-10-31 16:32:12 -0300

    Inicializacao do BD para testes

--- src/main/java/com/marcelolopes/cursomc/CursomcApplication.java
+++ src/main/java/com/marcelolopes/cursomc/CursomcApplication.java
@@ -1,19 +1,33 @@
 package com.marcelolopes.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import com.marcelolopes.cursomc.domain.Categoria;
+import com.marcelolopes.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
 @EntityScan(basePackages="com.marcelolopes.cursomc.domain")
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
 
-	/**
-	 * 
-	 * @param args
-	 */
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
--- src/main/java/com/marcelolopes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcelolopes/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,5 @@
 package com.marcelolopes.cursomc.resources;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
