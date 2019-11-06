ad57f1e1c9b86444b91b055be4f68c5990476f57
marcelolopesbr <marcelo.lopes@gmail.com>
2019-11-06 18:22:19 -0300

    Implementado Cidade e Cliente

--- src/main/java/com/marcelolopes/cursomc/CursomcApplication.java
+++ src/main/java/com/marcelolopes/cursomc/CursomcApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import com.marcelolopes.cursomc.domain.Categoria;
+import com.marcelolopes.cursomc.domain.Cidade;
+import com.marcelolopes.cursomc.domain.Estado;
 import com.marcelolopes.cursomc.domain.Produto;
 import com.marcelolopes.cursomc.repositories.CategoriaRepository;
+import com.marcelolopes.cursomc.repositories.CidadeRepository;
+import com.marcelolopes.cursomc.repositories.EstadoRepository;
 import com.marcelolopes.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -22,6 +26,13 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -47,6 +58,20 @@ public class CursomcApplication implements CommandLineRunner{
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado e1 = new Estado(null,"Minas Gerais");
+		Estado e2 = new Estado(null,"São Paulo");
+		
+		Cidade c1 = new Cidade(null,"Uberlândia",e1);
+		Cidade c2 = new Cidade(null,"Campinas",e2);
+		Cidade c3 = new Cidade(null,"São Paulo",e2);
+		
+		e1.getCidades().addAll(Arrays.asList(c1));
+		e2.getCidades().addAll(Arrays.asList(c2, c3));		
+		
+		estadoRepository.saveAll(Arrays.asList(e1, e2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
 		
 	}
 
---
+++ src/main/java/com/marcelolopes/cursomc/domain/Cliente.java
@@ -0,0 +1,13 @@
+package com.marcelolopes.cursomc.domain;
+
+import com.marcelolopes.cursomc.domain.enums.TipoCliente;
+
+public class Cliente {
+
+	private Integer id;
+	private String nome; 
+	private String email;
+	private String cpfOuCnpj;
+	private TipoCliente tipo;
+	
+}
