bd14c4e1effd1dffefecde7294bbc72aec1a81d2
marcelolopesbr <marcelo.lopes@gmail.com>
2019-11-13 14:33:48 -0300

    Validação customizada de CPF ou CNPJ

--- src/main/java/com/marcelolopes/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/marcelolopes/cursomc/dto/ClienteNewDTO.java
@@ -2,37 +2,51 @@ package com.marcelolopes.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.marcelolopes.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
-	private Integer id;
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="Nome deve ter entre 5 e 80 caracteres")
 	private String nome; 
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")	
 	private String email;
+
+	@NotEmpty(message="Preenchimento obrigatório")	
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cep;
 	
 	private Integer cidadeId;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
 	
 	public ClienteNewDTO() {
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
 	}
 
 	public String getNome() {
