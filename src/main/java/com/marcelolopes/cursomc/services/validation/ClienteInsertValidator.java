ef359de6b9714ae34435ccc2a1adff6aae92132e
marcelolopesbr <marcelo.lopes@gmail.com>
2019-11-13 14:46:13 -0300

    Validação customizada para email de Cliente duplicado

--- src/main/java/com/marcelolopes/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/marcelolopes/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.marcelolopes.cursomc.domain.enums.TipoCliente;
 import com.marcelolopes.cursomc.dto.ClienteNewDTO;
+import com.marcelolopes.cursomc.repositories.ClienteRepository;
 import com.marcelolopes.cursomc.resources.exceptions.FieldMessage;
 import com.marcelolopes.cursomc.services.validation.util.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +35,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo() == TipoCliente.PESSOAJURIDICA.getCod() 
 				&& ! BR.isValidTfn(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido"));
+		}
+		
+		if (clienteRepository.findByEmail(objDto.getEmail()) != null) {
+			list.add(new FieldMessage("email","Email duplicado"));
 		}
 		
 		for (FieldMessage e : list) {
